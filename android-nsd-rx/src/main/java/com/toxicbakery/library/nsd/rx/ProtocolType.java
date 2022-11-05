2217b1c6a1c81ba42025dd2ba6a651fb81be4e02
Patrick Geselbracht <patrick.geselbracht@grandcentrix.net>
2022-11-05 18:30:05 +0100

    Set ProtocolType retention policy to SOURCE

--- android-nsd-rx/src/main/java/com/toxicbakery/library/nsd/rx/ProtocolType.java
+++ android-nsd-rx/src/main/java/com/toxicbakery/library/nsd/rx/ProtocolType.java
@@ -1,13 +1,13 @@
 package com.toxicbakery.library.nsd.rx;
 
 import android.net.nsd.NsdManager;
+
 import androidx.annotation.IntDef;
 
+import kotlin.annotation.AnnotationRetention;
 import kotlin.annotation.Retention;
 
-@Retention()
-@IntDef({
-        NsdManager.PROTOCOL_DNS_SD
-})
+@Retention(value = AnnotationRetention.SOURCE)
+@IntDef({NsdManager.PROTOCOL_DNS_SD})
 public @interface ProtocolType {
 }
